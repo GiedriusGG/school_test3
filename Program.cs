ccacdf755e80951825cd75dfa4faa5aaf6f7e8d8
GiedriusGG <76166602+GiedriusGG@users.noreply.github.com>
2020-12-17 15:58:22 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace school_test3
+{
+    class Program
+    {
+        public static string Reverse( string s )
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse( charArray );
+            return new string( charArray );
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Task 3: Write a C# program that takes a word as an input and displays it reversed.");
+            Console.WriteLine("Please enter Test Data:");
+            var EnteredText = Console.ReadLine();
+            Console.WriteLine("Output: " + Reverse(EnteredText));
+        }
+
+    }
+}
